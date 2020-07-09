afdf3b9f5c6a4bdfde052c855991d75f560852e7
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-09 21:56:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,19 +1,8 @@
+require "pry"
+
 def unsafe?(speed)
-  if speed > 60
-    true
-  elsif speed < 40
-    true
-  elsif speed == 50
-    false
-  else 
+  if speed > 60 || speed < 40
+    return true
+  
   end
 end
-
-
-
-def not_safe?(speed)
-	speed > 60 || speed < 40 ? true : false
-end
-	
-
-
